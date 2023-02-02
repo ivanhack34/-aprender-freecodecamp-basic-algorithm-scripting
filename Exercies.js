abec96564131fb162bd04b57b57b58410260d62b
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-02-02 14:34:38 -0400

    Exercise of day 21 part 2: 2/2/2023
    Exercises 1 to 3 were done in this module

---
+++ Exercies.js
@@ -0,0 +1,52 @@
+//TODO: Exercise of day 21 part 2: 2/2/2023
+
+//?1-Convert Celsius to Fahrenheit
+
+/*
+function convertCtoF(celsius) {
+  let fahrenheit;
+  return fahrenheit = (celsius * 9/5) + 32;
+  //This will make using the formula from Celsius to Fahrenheit I can see the result of how much it gives me in Fahrenheit
+}
+
+convertCtoF(30);
+
+console.log(convertCtoF(30));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+*/
+
+//?2-Reverse a String
+
+/*
+function reverseString(str) {
+  return str.split('').reverse().join('');
+   //This will make the split() method devide each position of each character
+   //With the reverse() method, I will invert the characters of the array
+   //Finally, with the join() method, it will unite all the elements in a text string
+}
+
+console.log(reverseString("hello"));
+*/
+
+//?3-Factorialize a Number
+
+/*
+function factorialize(num) {
+    let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+  //First we create a variable that is equal to 1. Then we create a for loop. 
+  //Where each value that starts at 1 and is less than or equal to the number and is iterable. 
+  //It will give the multiplication of that number in factorial form. Example: If the number is: 10. 
+  //It will multiply: 1*2*3*4*5*6*7*8*9*10 = 3628800.
+}
+
+console.log(factorialize(5));
+console.log(factorialize(10));
+console.log(factorialize(20));
+*/
+
+//?4-Find the Longest Word in a String
